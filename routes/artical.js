7fee3af1bca0f3091bc934ce343c16496be3ee90
Vic Zhang <f160972@efivestar.com>
2017-11-28 15:02:04 +0800

    增加文章列表

--- routes/artical.js
+++ routes/artical.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var PageModel = require('../models/page');
 
 router.get('/', (req, res, next) => {
-  res.render('articalList');
+  PageModel.find((err, data) => {
+    console.log('data', data);
+    res.render('articalList', { title: '首页', userName: req.cookies.user, pageList: data});    
+  });
 });
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,8 @@ var UserModel = require('../models/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('UserModel', UserModel);
   UserModel.find((err, user) => {
     res.send(user);
-    
   });
 });
 
